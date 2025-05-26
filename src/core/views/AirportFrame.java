758c7e6250a9315d5ee540d7ab46810ccb639f0e
Alfred-0705 <adbadillo@uninorte.edu.co>
2025-05-26 00:02:48 -0500

    carga de archivos JSON en la interfaz

--- src/core/views/AirportFrame.java
+++ src/core/views/AirportFrame.java
@@ -1906,21 +1906,7 @@ if (response.getStatus() >= 500) {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        System.setProperty("flatlaf.useNativeLibrary", "false");
-
-        try {
-            UIManager.setLookAndFeel(new FlatDarkLaf());
-        } catch (Exception ex) {
-            System.err.println("Failed to initialize LaF");
-        }
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new AirportFrame().setVisible(true);
-            }
-        });
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Add;
